f7266f96d6a55203fb7cbd7741f00984131eb034
Diógenes Castro <diogenesxmarlon@gmail.com>
2020-11-16 11:22:19 -0300

    Regexs para Email e CPF Adicionados

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -123,13 +123,16 @@ class UserController extends Controller
         }
     }
 
-    // Rules used for request validation
+    // Rules for request validation
     public function rules() {
+        $email_regex = '/(?:[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/';
+        $cpf_regex = '/\d{3}\.\d{3}\.\d{3}\-\d{2}/';
+
         return [
             'name' => ['required', 'string', 'max:255'],
-            'cpf' => ['required', 'string', 'max:255'],
+            'cpf' => ['required', 'string', 'max:255', 'regex:' . $cpf_regex],
             'birthday' => ['required', 'date'],
-            'email' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'max:255', 'regex:' . $email_regex],
             'phone_number' => ['required', 'string', 'max:255'],
             'address' => ['required', 'string', 'max:255'],
             'city' => ['required', 'string', 'max:255'],
